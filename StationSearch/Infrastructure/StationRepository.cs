d6b41aafaf65d00c2789a921d436fb5a89fed3f6
tashinoki <kenya.112163@gmail.com>
2022-05-01 20:37:12 +0900

    becase AngleSearch dose not work on browser

---
+++ StationSearch/Infrastructure/StationRepository.cs
@@ -0,0 +1,33 @@
+﻿using StationSearch.Models;
+
+namespace StationSearch.Infrastructure
+{
+    public interface IStationRepository
+    {
+        public Task<string> GetStationAsync(string name, PrefectureCode code);
+    }
+
+    public class StationRepository : IStationRepository
+    {
+        private readonly static HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://express.heartrails.com"),
+            Timeout = TimeSpan.FromSeconds(300)
+        };
+
+        public async Task<string> GetStationAsync(string name, PrefectureCode code)
+
+        {
+            var param = new Dictionary<string, string>()
+            {
+                { "method", "getStations" },
+                { "name", name },
+                { "prefecture", "東京都" }
+            };
+
+            var response = await _httpClient.GetAsync($"/api/json?{ await new FormUrlEncodedContent(param).ReadAsStringAsync() }");
+            var content = await response.Content.ReadAsStringAsync();
+            return string.Empty;
+        }
+    }
+}
---
+++ StationSearch/UseCases/StationUseCase.cs
@@ -0,0 +1,26 @@
+﻿using StationSearch.Services;
+
+namespace StationSearch.UseCases
+{
+    interface IStationUseCase
+    {
+        public Task SearchStationAsync(string name);
+    }
+
+    public class StationUseCase : IStationUseCase
+    {
+        private readonly ITrainTransitService _trainTrainsitService;
+
+        public StationUseCase(
+            ITrainTransitService trainTransitService)
+        {
+            _trainTrainsitService = trainTransitService;
+        }
+
+        public async Task SearchStationAsync(string name)
+        {
+            await _trainTrainsitService.SearchTrainTransitAsync(name, "渋谷");
+            return;
+        }
+    }
+}
