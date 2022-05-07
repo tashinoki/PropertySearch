63b011ec2ed52384968243d3f9ac16837ce213fe
kino <45783740+tashinoki@users.noreply.github.com>
2022-05-07 22:22:00 +0900

    what is ubiquitous language

---
+++ StationSearch/Models/TrainTransit.cs
@@ -0,0 +1,16 @@
+﻿using StationSearch.Services.TrainTransit;
+
+namespace StationSearch.Models
+{
+    public class TrainTransit
+    {
+        private readonly IReadOnlyList<TrainTransitWay> _trainTransits;
+        
+        public TrainTransit(IReadOnlyList<TrainTransitWay> trainTransits)
+        {
+            _trainTransits = trainTransits;
+        }
+        public Station Distination
+            => _trainTransits.Last().DstStation;
+    }
+}
